c0b259f0351390412e1301fa7539c2114c2a5610
woojin <tkdalsrb147@naver.com>
2025-05-16 00:05:44 +0900

    initial commit

---
+++ src/test/java/com/example/homeaid/HomeAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
