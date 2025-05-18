363c4a4f130d649a95701301b8b30ad9aa72ab07
woojin <tkdalsrb147@naver.com>
2025-05-18 21:20:54 +0900

    feat: Swagger setting
    - add swagger build
    - creat SwaggerConfig.java

--- src/main/java/com/example/homeaid/global/common/response/ApiResponse.java
+++ src/main/java/com/example/homeaid/global/common/response/ApiResponse.java
@@ -1,21 +1,27 @@
 package com.example.homeaid.global.common.response;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "공통 API 응답 포맷")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 public class ApiResponse<T> {
 
+  @Schema(description = "성공 여부", example = "true")
   private boolean success;
 
+  @Schema(description = "응답 데이터", nullable = true)
   private T data;
 
+  @Schema(description = "응답 코드", example = "SUCCESS", nullable = true)
   private String code;
 
+  @Schema(description = "응답 메시지", example = "요청이 성공했습니다.", nullable = true)
   private String message;
 
   public static <T> ApiResponse<T> success(T data) {
