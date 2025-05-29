afb90c3834b71f07680d11cb1d59562b9b9feba6
soohoioa <151589776+soohoioa@users.noreply.github.com>
2025-05-29 18:51:56 +0900

    Revert "chore: payment build 에 reservation 추가"

--- payment/build.gradle.kts
+++ payment/build.gradle.kts
@@ -13,7 +13,6 @@ tasks.named<Jar>("jar") {
 }
 
 dependencies {
-    implementation(project(":reservation"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
