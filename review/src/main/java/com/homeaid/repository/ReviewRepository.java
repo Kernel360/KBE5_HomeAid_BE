20146ef56d65365fea98911ece796549e0eee963
강희진 <khjin0115@gmail.com>
2025-06-03 22:15:51 +0900

    refactor: 사용하지 않는 import 제거 및 코드 정렬

--- review/src/main/java/com/homeaid/repository/ReviewRepository.java
+++ review/src/main/java/com/homeaid/repository/ReviewRepository.java
@@ -2,7 +2,6 @@ package com.homeaid.repository;
 
 
 import com.homeaid.domain.Review;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
