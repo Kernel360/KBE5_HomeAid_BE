ff6110bf124c5a0e40401f5c44c55911af45494d
woojin <tkdalsrb147@naver.com>
2025-06-07 13:45:55 +0900

    refactor: rebase

--- reservation/src/main/java/com/homeaid/service/ReservationService.java
+++ reservation/src/main/java/com/homeaid/service/ReservationService.java
@@ -14,9 +14,13 @@ public interface ReservationService {
 
   Reservation updateReservation(Long reservationId, Long userId, Reservation reservation, Long serviceSubOptionId);
 
-  void deleteReservation(Long id);
-
   Page<Reservation> getReservations(Pageable pageable);
 
   void deleteReservation(Long reservationId, Long userId);
+
+  Page<Reservation> getReservationsByCustomer(Long userId, Pageable pageable);
+
+  Page<Reservation> getReservationsByManager(Long userId, Pageable pageable);
+
+
 }
