09268becd5423c64446f4dc6130d59ff8bcf6581
woojin <tkdalsrb147@naver.com>
2025-06-13 10:11:29 +0900

    refactor: check in out 로직 수정

--- worklog/src/main/java/com/homeaid/repository/WorkLogRepository.java
+++ worklog/src/main/java/com/homeaid/repository/WorkLogRepository.java
@@ -1,6 +1,7 @@
 package com.homeaid.repository;
 
 import com.homeaid.domain.WorkLog;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ public interface WorkLogRepository extends JpaRepository<WorkLog, Long> {
   boolean existsWorkLogByManagerIdAndReservationId(Long managerId, Long reservationId);
 
   Page<WorkLog> findAllByManagerId(Long userId, Pageable pageable);
+
+  Optional<WorkLog> findByReservationId(Long reservationId);
 }
--- worklog/src/main/java/com/homeaid/service/WorkLogServiceImpl.java
+++ worklog/src/main/java/com/homeaid/service/WorkLogServiceImpl.java
@@ -1,10 +1,14 @@
 package com.homeaid.service;
 
+import com.homeaid.domain.Matching;
 import com.homeaid.domain.Reservation;
 import com.homeaid.domain.WorkLog;
+import com.homeaid.domain.enumerate.WorkType;
 import com.homeaid.exception.CustomException;
+import com.homeaid.exception.MatchingErrorCode;
 import com.homeaid.exception.ReservationErrorCode;
 import com.homeaid.exception.WorkLogErrorCode;
+import com.homeaid.repository.MatchingRepository;
 import com.homeaid.repository.ReservationRepository;
 import com.homeaid.repository.WorkLogRepository;
 import com.homeaid.util.GeoUtils;
@@ -18,77 +22,80 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class WorkLogServiceImpl implements WorkLogService {
-    private final WorkLogRepository workLogRepository;
-    private final ReservationRepository reservationRepository;
-    private final static int CHECK_RANGE_DISTANCE_METER = 500; //500미터
 
-    @Transactional
-    @Override
-    public WorkLog createWorkLog(WorkLog workLog, Long reservationId, Double latitude, Double longitude) {
-        //요청 한 예약건이 존재하는 예약건인지 검증
-        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(()
-                -> new CustomException(ReservationErrorCode.RESERVATION_NOT_FOUND));
-        //존재한 예약건에 대해 요청한 매니저 아이디가 체크인을 한적이있는지 검증
-        if (workLogRepository.existsWorkLogByManagerIdAndReservationId(workLog.getManagerId(), reservationId)) {
-            throw new CustomException(WorkLogErrorCode.ALREADY_COMPLETED_CHECKIN);
-        }
+  private final WorkLogRepository workLogRepository;
+  private final ReservationRepository reservationRepository;
+  private final static int CHECK_RANGE_DISTANCE_METER = 500; //500미터
+  private final MatchingRepository matchingRepository;
 
-        if (!isValidDistance(reservationId, latitude, longitude)) {
-            throw new CustomException(WorkLogErrorCode.OUT_OF_WORK_RANGE);
-        }
-        workLog.assignReservation(reservation);
-
-        return workLogRepository.save(workLog);
+  @Transactional
+  @Override
+  public WorkLog createWorkLog(Long userId, Long reservationId, Double latitude, Double longitude) {
+    //요청 한 예약건이 존재하는 예약건인지 검증
+    Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(()
+        -> new CustomException(ReservationErrorCode.RESERVATION_NOT_FOUND));
+    //존재한 예약건에 대해 요청한 매니저 아이디가 체크인을 한적이있는지 검증
+    if (workLogRepository.existsWorkLogByManagerIdAndReservationId(userId, reservationId)) {
+      throw new CustomException(WorkLogErrorCode.ALREADY_COMPLETED_CHECKIN);
     }
 
-    @Transactional
-    @Override
-    public WorkLog updateWorkLogForCheckOut(WorkLog requestWorkLog, Long requestWorkLogId, Double latitude, Double longitude) {
-        WorkLog workLog = workLogRepository.findById(requestWorkLogId).orElseThrow(() ->
-              new CustomException(WorkLogErrorCode.WORKLOG_NOT_FOUND)
-        );
+    if (!isValidDistance(reservationId, latitude, longitude)) {
+      throw new CustomException(WorkLogErrorCode.OUT_OF_WORK_RANGE);
+    }
+    WorkLog workLog = WorkLog.builder().workType(WorkType.CHECKIN).managerId(userId)
+        .reservation(reservation).build();
 
-        isValidManager(workLog.getId(), requestWorkLog.getManagerId());
+    return workLogRepository.save(workLog);
+  }
 
-        if (!isValidDistance(workLog.getReservation().getId(), latitude, longitude)) {
-            throw new CustomException(WorkLogErrorCode.OUT_OF_WORK_RANGE);
-        }
-        workLog.updateCheckOutTime(requestWorkLog);
+  @Transactional
+  @Override
+  public void updateWorkLogForCheckOut(Long userId, Long reservationId,
+      Double latitude, Double longitude) {
 
-        updateReservationStatusCompleted(workLog.getReservation());
+    WorkLog workLog = isValidManager(reservationId, userId);
 
-        return workLog;
+    if (!isValidDistance(workLog.getReservation().getId(), latitude, longitude)) {
+      throw new CustomException(WorkLogErrorCode.OUT_OF_WORK_RANGE);
     }
 
-    @Override
-    @Transactional(readOnly = true)
-    public Page<WorkLog> getAllWorkLogsByManager(Long userId, Pageable pageable) {
-        return workLogRepository.findAllByManagerId(userId, pageable);
+    workLog.updateCheckOut();
+
+    updateReservationStatusCompleted(workLog.getReservation());
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public Page<WorkLog> getAllWorkLogsByManager(Long userId, Pageable pageable) {
+    return workLogRepository.findAllByManagerId(userId, pageable);
+  }
+
+  /**
+   * @param reservationId 예약 위치 조회할 id
+   * @return 예약 위치와 체크인 위치의 차이가 CHECK_RANGE_DISTANCE_METER 범위 안에 있으면 true
+   */
+  private boolean isValidDistance(Long reservationId, Double latitude, Double longitude) {
+    Reservation reservation = reservationRepository.findById(reservationId)
+        .orElseThrow(() -> new CustomException(ReservationErrorCode.RESERVATION_NOT_FOUND));
+    double calculatedDistance = GeoUtils.calculateDistanceInMeters(reservation.getLatitude(),
+        reservation.getLongitude(), latitude, longitude);
+
+    return calculatedDistance < CHECK_RANGE_DISTANCE_METER;
+  }
+
+  public WorkLog isValidManager(Long reservationId, Long requestManagerId) {
+    WorkLog workLog = workLogRepository.findByReservationId(reservationId).orElseThrow(() -> new CustomException(ReservationErrorCode.RESERVATION_NOT_FOUND));
+
+    if (!workLog.getManagerId().equals(requestManagerId)) {
+      throw new CustomException(WorkLogErrorCode.CHECKOUT_MANAGER_MISMATCH);
     }
 
-    /**
-     * @param reservationId 예약 위치 조회할 id
-     * @return 예약 위치와 체크인 위치의 차이가 CHECK_RANGE_DISTANCE_METER 범위 안에 있으면 true
-     */
-    private boolean isValidDistance(Long reservationId, Double latitude, Double longitude) {
-        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new CustomException(ReservationErrorCode.RESERVATION_NOT_FOUND));
-        double calculatedDistance = GeoUtils.calculateDistanceInMeters(reservation.getLatitude(), reservation.getLongitude(), latitude, longitude);
+    return workLog;
+  }
 
-        return calculatedDistance < CHECK_RANGE_DISTANCE_METER;
-    }
-
-    public void isValidManager(Long workLogId, Long requestManagerId) {
-        WorkLog workLog = workLogRepository.findById(workLogId)
-                .orElseThrow(() -> new CustomException(WorkLogErrorCode.WORKLOG_NOT_FOUND));
-
-        if (!workLog.getManagerId().equals(requestManagerId)) {
-            throw new CustomException(WorkLogErrorCode.CHECKOUT_MANAGER_MISMATCH);
-        }
-    }
-
-    public void updateReservationStatusCompleted(Reservation reservation) {
-        reservation.updateStatusCompleted();
-    }
+  public void updateReservationStatusCompleted(Reservation reservation) {
+    reservation.updateStatusCompleted();
+  }
 
 
 }
