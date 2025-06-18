6572f0cf0b80b4c32051872e4dbbea1d2f96596d
anjihyeon <jhan-@naver.com>
2025-06-18 17:16:00 +0900

    chore: dto 필수값 추가

--- admin/src/main/java/com/homeaid/serviceoption/dto/request/ServiceOptionRequestDto.java
+++ admin/src/main/java/com/homeaid/serviceoption/dto/request/ServiceOptionRequestDto.java
@@ -3,7 +3,9 @@ package com.homeaid.serviceoption.dto.request;
 import com.homeaid.serviceoption.domain.ServiceOption;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
+
 import lombok.*;
 
 @Getter
@@ -12,15 +14,19 @@ import lombok.*;
 @Schema(description = "서비스 상위 옵션 등록/수정 요청 DTO")
 public class ServiceOptionRequestDto {
 
-  @NotBlank
-  @Schema(description = "옵션 이름", example = "청소")
-  private String name;
+    @NotBlank
+    @Schema(description = "옵션 이름", example = "청소")
+    private String name;
 
-  @Schema(description = "옵션에서 제공하는 서비스", example = "['배란다 청소', '욕실 청소', '창문 청소']")
-  private List<String> features;
+    @Schema(description = "옵션에서 제공하는 서비스", example = "[\"배란다 청소\", \"욕실 청소\", \"창문 청소\"]")
+    private List<String> features;
 
-  public static ServiceOption toEntity(ServiceOptionRequestDto serviceSubOptionRequestDto) {
-    return ServiceOption.builder().name(serviceSubOptionRequestDto.getName())
-        .features(serviceSubOptionRequestDto.getFeatures()).build();
-  }
+    @Schema(description = "서비스 가격", example = "30000")
+    private Integer price;
+
+    public static ServiceOption toEntity(ServiceOptionRequestDto serviceSubOptionRequestDto) {
+        return ServiceOption.builder().name(serviceSubOptionRequestDto.getName())
+                .price(serviceSubOptionRequestDto.getPrice())
+                .features(serviceSubOptionRequestDto.getFeatures()).build();
+    }
 }
