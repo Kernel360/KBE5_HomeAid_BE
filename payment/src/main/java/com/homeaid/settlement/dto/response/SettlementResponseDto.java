7572e169662843b254f871f326f9402d2c3839c4
soohoioa <soohoioa@gmail.com>
2025-06-20 19:06:00 +0900

    fix: 관리자 결제, 장산 dto 추가

--- payment/src/main/java/com/homeaid/settlement/dto/response/SettlementResponseDto.java
+++ payment/src/main/java/com/homeaid/settlement/dto/response/SettlementResponseDto.java
@@ -10,19 +10,23 @@ import lombok.Getter;
 @Builder
 public class SettlementResponseDto {
 
+  private Long id;
   private Long managerId;
   private LocalDate from;
   private LocalDate to;
   private Integer totalAmount; // 총 결제금액
   private Integer managerAmount; // 매니저 80
   private Integer adminAmount; // 관리자 20 DB 저장 X
-  private LocalDateTime settledAt;
+  private LocalDateTime settledAt; // 정산 생성일
+  private LocalDateTime confirmedAt; // 관리자 승인일
+  private LocalDateTime paidAt;      // 관리자 지급일
 
   public static SettlementResponseDto from(Settlement s) {
     Integer managerAmount = (int) Math.round(s.getTotalAmount() * 0.8);
     Integer adminAmount = s.getTotalAmount() - managerAmount;
 
     return SettlementResponseDto.builder()
+        .id(s.getId())
         .managerId(s.getManagerId())
         .from(s.getSettlementWeekStart())
         .to(s.getSettlementWeekEnd())
@@ -30,6 +34,8 @@ public class SettlementResponseDto {
         .managerAmount(managerAmount)
         .adminAmount(adminAmount)
         .settledAt(s.getSettledAt())
+        .confirmedAt(s.getConfirmedAt())
+        .paidAt(s.getPaidAt())
         .build();
   }
 
