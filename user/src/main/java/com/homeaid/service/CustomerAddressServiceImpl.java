2bd8ac5f4e1fb71a7b474fd1f947b7f936b1e7b3
woojin <tkdalsrb147@naver.com>
2025-06-20 13:48:13 +0900

    refactor: 안쓰는 코드 삭제

--- user/src/main/java/com/homeaid/service/CustomerAddressServiceImpl.java
+++ user/src/main/java/com/homeaid/service/CustomerAddressServiceImpl.java
@@ -2,22 +2,18 @@ package com.homeaid.service;
 
 import com.homeaid.domain.Customer;
 import com.homeaid.domain.CustomerAddress;
-import com.homeaid.dto.response.CustomerAddressResponseDto;
 import com.homeaid.exception.CustomException;
 import com.homeaid.exception.UserErrorCode;
 import com.homeaid.repository.CustomerAddressRepository;
 import com.homeaid.repository.CustomerRepository;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
+
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class CustomerAddressServiceImpl implements CustomerAddressService {
 
   private final CustomerAddressRepository customerAddressRepository;
@@ -26,6 +22,7 @@ public class CustomerAddressServiceImpl implements CustomerAddressService {
 
 
   @Override
+  @Transactional(readOnly = true)
   public List<CustomerAddress> getSavedAddresses(Long customerId) {
 
     return customerAddressRepository.findByCustomerIdOrderByIdDesc(
@@ -33,8 +30,8 @@ public class CustomerAddressServiceImpl implements CustomerAddressService {
   }
 
 
-  @Transactional
   @Override
+  @Transactional
   public CustomerAddress saveAddress(Long customerId, CustomerAddress customerAddress) {
 
     Customer customer = customerRepository.findById(customerId)
@@ -68,8 +65,8 @@ public class CustomerAddressServiceImpl implements CustomerAddressService {
   }
 
 
-  @Transactional
   @Override
+  @Transactional
   public void deleteAddress(Long customerId, Long addressId) {
 
     CustomerAddress address = customerAddressRepository.findByCustomerIdAndId(customerId,
@@ -96,20 +93,4 @@ public class CustomerAddressServiceImpl implements CustomerAddressService {
     }
   }
 
-  // 수정용 중복 주소 검증
-  private void validateDuplicateAddressForUpdate(Long customerId, Long addressId,
-      CustomerAddress requestDto) {
-    List<CustomerAddress> existingAddresses = customerAddressRepository.findByCustomerIdOrderByIdDesc(
-        customerId);
-
-    boolean isDuplicate = existingAddresses.stream()
-        .filter(addr -> !addr.getId().equals(addressId)) // 현재 수정하는 주소는 제외
-        .anyMatch(addr -> addr.getAddress().equals(requestDto.getAddress())
-            && (addr.getAddressDetail() != null ? addr.getAddressDetail()
-            .equals(requestDto.getAddressDetail()) : requestDto.getAddressDetail() == null));
-
-    if (isDuplicate) {
-      throw new CustomException(UserErrorCode.DUPLICATE_ADDRESS);
-    }
-  }
 }
