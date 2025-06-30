42b3458d2ee80646c7cecd8be408f2474432aebc
강희진 <khjin0115@gmail.com>
2025-06-30 12:09:25 +0900

    fix: 수정, 삭제 에러 해결

--- reservation/src/main/java/com/homeaid/dto/response/ServiceIssueResponseDto.java
+++ reservation/src/main/java/com/homeaid/dto/response/ServiceIssueResponseDto.java
@@ -3,6 +3,8 @@ package com.homeaid.dto.response;
 import com.homeaid.domain.ServiceIssue;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,8 +22,22 @@ public class ServiceIssueResponseDto {
   @Schema(description = "이슈 내용")
   private String content;
 
+  @Schema(description = "이슈 이미지 목록")
+  private List<ImageDto> images;
+
   @Schema(description = "이슈 작성일")
   private LocalDateTime createdAt;
+
+  @Getter
+  @Builder
+  @Schema(description = "서비스 이슈 이미지")
+  public static class ImageDto {
+    @Schema(description = "이미지 원본 파일명")
+    private String originalName;
+
+    @Schema(description = "이미지 URL")
+    private String url;
+  }
 
   public static ServiceIssueResponseDto toDto(ServiceIssue serviceIssue) {
     return ServiceIssueResponseDto.builder()
@@ -29,7 +45,12 @@ public class ServiceIssueResponseDto {
         .reservationId(serviceIssue.getReservation().getId())
         .content(serviceIssue.getContent())
         .createdAt(serviceIssue.getCreatedAt())
+        .images(serviceIssue.getImages().stream()
+            .map(image -> ImageDto.builder()
+                .originalName(image.getOriginalName())
+                .url(image.getUrl())
+                .build())
+            .collect(Collectors.toList()))
         .build();
   }
-
 }
--- reservation/src/main/java/com/homeaid/service/ServiceIssueFileService.java
+++ reservation/src/main/java/com/homeaid/service/ServiceIssueFileService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ServiceIssueFileService {
+
   void uploadFiles(ServiceIssue serviceIssue, List<MultipartFile> files);
+
   void updateFiles(ServiceIssue serviceIssue, List<MultipartFile> files);
+
   void deleteFiles(ServiceIssue serviceIssue);
 }
