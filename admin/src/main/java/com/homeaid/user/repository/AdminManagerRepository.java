c8fd0d17e1a70c7d80fd211b159a6daf3fb0dde9
soohoioa <soohoioa@gmail.com>
2025-07-10 21:25:43 +0900

    fix: 검색기능수정

--- admin/src/main/java/com/homeaid/user/repository/AdminManagerRepository.java
+++ admin/src/main/java/com/homeaid/user/repository/AdminManagerRepository.java
@@ -5,16 +5,11 @@ import com.homeaid.domain.enumerate.ManagerStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminManagerRepository extends JpaRepository<Manager, Long> {
-
-  Page<Manager> findByNameContainingIgnoreCase(String name, Pageable pageable);
-
-  Page<Manager> findByPhoneContaining(String phone, Pageable pageable);
-
-  Page<Manager> findByCareerContaining(String career, Pageable pageable);
-
-  Page<Manager> findByStatus(ManagerStatus status, Pageable pageable);
+public interface AdminManagerRepository extends JpaRepository<Manager, Long>,
+    JpaSpecificationExecutor<Manager> {
 }
+
